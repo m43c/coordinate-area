1b35151596f9a412fc5d9cbb00a3378ac2e3c967
m43c <marqencinas@gmail.com>
2023-10-25 21:10:00 -0400

    Add function to calculate area

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import Marker from "./CustomMarker";
+import * as turf from "@turf/turf";
 
 export default function Map() {
     const [currentLocation, setcurrentLocation] = useState({
@@ -30,6 +31,9 @@ export default function Map() {
         longitude: -63.687059,
     });
 
+    // State controller for movement of markers
+    const [moveMarkers, setmoveMarkers] = useState(0);
+
     /**
      * * Function used to handle the onDragEnd event that occurs when a dragged
      * * marker is released on the map.
@@ -38,7 +42,37 @@ export default function Map() {
      */
     const handleMarkerDragEnd = (coordinate, pointSetter) => {
         pointSetter(coordinate);
+        setmoveMarkers(moveMarkers + 1);
     };
+
+    const calculateArea = () => {
+        if (moveMarkers === 4) {
+            // Define the four geographic coordinates (latitude and longitude) of your points
+            const coordinate1 = turf.point([point1.longitude, point1.latitude]);
+            const coordinate2 = turf.point([point2.longitude, point2.latitude]);
+            const coordinate3 = turf.point([point3.longitude, point3.latitude]);
+            const coordinate4 = turf.point([point4.longitude, point4.latitude]);
+
+            // Create a polygon from these points
+            const polygon = turf.polygon([
+                [
+                    turf.getCoord(coordinate1),
+                    turf.getCoord(coordinate2),
+                    turf.getCoord(coordinate3),
+                    turf.getCoord(coordinate4),
+                    turf.getCoord(coordinate1),
+                ],
+            ]);
+
+            // Calculate the area of the polygon
+            const area = turf.area(polygon);
+            console.log(`Approximate area in square meters: ${area}`);
+        }
+    };
+
+    useEffect(() => {
+        calculateArea();
+    }, [point1, point2, point3, point4]);
 
     return (
         <MapView
